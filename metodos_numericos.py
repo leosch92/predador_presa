75c1fcca3453ae03d7e6d9b85391299ca4db5108
leosch92 <leonardo_schimpf@id.uff.br>
2018-11-01 11:31:03 -0300

    Pequenas alterações para adaptação do código

--- metodos_numericos.py
+++ metodos_numericos.py
@@ -2,9 +2,9 @@ from predador_presa import PredadorPresa
 
 
 def runge_kutta_2(pp_k, delta_t):
-    pp_kmaismeio = euler_modificado(pp_k, delta_t)
-    presa = pp_k.presa + delta_t * pp_kmaismeio.f_presa()
-    predador = pp_k.predador + delta_t * pp_kmaismeio.f_predador()
+    pp_k_mais_meio = euler_modificado(pp_k, delta_t)
+    presa = pp_k.presa + delta_t * pp_k_mais_meio.f_presa()
+    predador = pp_k.predador + delta_t * pp_k_mais_meio.f_predador()
     return PredadorPresa(presa, predador, pp_k.t + 1)
 
 
@@ -14,13 +14,14 @@ def euler_modificado(pp_k, delta_t):
     return PredadorPresa(presa, predador, pp_k.t + 0.5)
 
 
-# Quando pp_kmaisum não é passado, temos que fazer uma predição com Runge-Kutta. Caso contrário, se trata de uma correção
-def backward_differentiation_formula_3(lista_pp, delta_t, pp_kmaisum=None):
+# Quando pp_k_mais_um não é passado, temos que fazer uma predição com Runge-Kutta.
+# Caso contrário, se trata de uma correção
+def backward_differentiation_formula_3(lista_pp, delta_t, pp_k_mais_um=None):
     flag_predicao = False
-    if pp_kmaisum is None:
-        pp_kmaisum = runge_kutta_2(lista_pp[2], delta_t)
+    if pp_k_mais_um is None:
+        pp_k_mais_um = runge_kutta_2(lista_pp[2], delta_t)
         flag_predicao = True
 
-    presa = (18 * lista_pp[2].presa - 9 * lista_pp[1].presa + 2 * lista_pp[0].presa + 6 * delta_t * pp_kmaisum.f_presa())/11.0
-    predador = (18 * lista_pp[2].predador - 9 * lista_pp[1].predador + 2 * lista_pp[0].predador + 6 * delta_t * pp_kmaisum.f_predador())/11.0
+    presa = (18 * lista_pp[2].presa - 9 * lista_pp[1].presa + 2 * lista_pp[0].presa + 6 * delta_t * pp_k_mais_um.f_presa()) / 11.0
+    predador = (18 * lista_pp[2].predador - 9 * lista_pp[1].predador + 2 * lista_pp[0].predador + 6 * delta_t * pp_k_mais_um.f_predador()) / 11.0
     return PredadorPresa(presa, predador, lista_pp[2].t + 1 if flag_predicao else lista_pp[2].t)
