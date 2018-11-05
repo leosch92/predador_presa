a3a5435cb17599c8da15f1d0e7b7496441c1d7d7
leosch92 <leonardo_schimpf@id.uff.br>
2018-11-05 13:08:38 -0200

    Implementado solucionador para código main ter menos responsabilidades

--- __main__.py
+++ __main__.py
@@ -1,10 +1,9 @@
-from predador_presa import PredadorPresa
+from solucionador import Solucionador
 from metodos_numericos import runge_kutta_2, backward_differentiation_formula_3, adams_moulton_3
-from copy import copy
 
 
 def main():
-    constantes_temporais = define_constantes_temporais()
+    constantes_temporais = {'t_final': 100.0, 't_inicial': 0.0, 'delta_t': 0.1}
     numero_correcoes = 2
     numero_iteracoes = calcula_numero_iteracoes(constantes_temporais)
     populacao_presa_inicial, populacao_predador_inicial = 1000, 100
@@ -14,18 +13,18 @@ def main():
 
     while loop:
         if opcao == 1:
-            inicializacoes_necessarias = 2
-            lista_pp = inicializa(constantes_temporais['delta_t'], populacao_presa_inicial, populacao_predador_inicial,
-                                  inicializacoes_necessarias, runge_kutta_2)
-            calcula_passos(constantes_temporais['delta_t'], lista_pp, numero_iteracoes, inicializacoes_necessarias,
-                           numero_correcoes, backward_differentiation_formula_3, runge_kutta_2)
+            solucionador = Solucionador(constantes_temporais['delta_t'],
+                                        (populacao_presa_inicial, populacao_predador_inicial),
+                                        runge_kutta_2, backward_differentiation_formula_3, runge_kutta_2,
+                                        numero_iteracoes, numero_correcoes, inicializacoes_necessarias=2)
+            solucionador.resolve()
             opcao = captura_input()
         elif opcao == 2:
-            inicializacoes_necessarias = 1
-            lista_pp = inicializa(constantes_temporais['delta_t'], populacao_presa_inicial, populacao_predador_inicial,
-                                  inicializacoes_necessarias, runge_kutta_2)
-            calcula_passos(constantes_temporais['delta_t'], lista_pp, numero_iteracoes, inicializacoes_necessarias,
-                           numero_correcoes, adams_moulton_3, runge_kutta_2)
+            solucionador = Solucionador(constantes_temporais['delta_t'],
+                                        (populacao_presa_inicial, populacao_predador_inicial),
+                                        runge_kutta_2, adams_moulton_3, runge_kutta_2,
+                                        numero_iteracoes, numero_correcoes, inicializacoes_necessarias=1)
+            solucionador.resolve()
             opcao = captura_input()
         elif opcao == 9:
             loop = False
@@ -41,42 +40,8 @@ def captura_input():
         return 0
 
 
-def define_constantes_temporais():
-    return {'t_final': 100.0, 't_inicial': 0.0, 'delta_t': 0.1}
-
-
 def calcula_numero_iteracoes(ct):
     return int((ct['t_final'] - ct['t_inicial'])/ct['delta_t'])
-
-
-def inicializa(delta_t, pop_presa, pop_predador, inicializacoes, metodo):
-    lista_pp = [PredadorPresa(pop_presa, pop_predador, 0)]
-    for i in range(inicializacoes):
-        pp = metodo(lista_pp[i], delta_t)
-        pp.t += 1
-        lista_pp.append(pp)
-    for pp in lista_pp:
-        pp.imprime()
-    return lista_pp
-
-
-def calcula_passos(delta_t, lista_pp, n_iter, inicializacoes, n_correcoes, metodo_calculador, metodo_preditor):
-    for i in range(n_iter - (inicializacoes + 1)):
-        pp_k_mais_um = metodo_calculador(lista_pp, delta_t, metodo_preditor=metodo_preditor)
-        pp_k_mais_um = corrige(lista_pp, pp_k_mais_um, n_correcoes, delta_t, metodo_calculador)
-        pp_k_mais_um.t += 1
-        lista_pp_copia = []
-        for j in range(len(lista_pp) - 1):
-            lista_pp_copia.append(lista_pp[j + 1])
-        lista_pp_copia.append(pp_k_mais_um)
-        lista_pp = copy(lista_pp_copia)
-        pp_k_mais_um.imprime()
-
-
-def corrige(lista_pp, pp_k_mais_um, n_correcoes, delta_t, metodo):
-    for j in range(n_correcoes):
-        pp_k_mais_um = metodo(lista_pp, delta_t, pp_k_mais_um)
-    return pp_k_mais_um
 
 
 if __name__ == "__main__":
--- predador_presa.py
+++ predador_presa.py
@@ -5,12 +5,10 @@ class PredadorPresa(object):
         self.predador = predador
         self.t = t
 
-    # Método que aproxima a derivada da presa
     def f_presa(self):
         alpha, beta = 0.8, 0.004
         return alpha * self.presa - beta * self.presa * self.predador
 
-    # Método que aproxima a derivada do predador
     def f_predador(self):
         gama, delta = 0.8, 0.001
         return -gama * self.predador + delta * self.presa * self.predador
