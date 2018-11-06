699cabc9a06c0252acbb3d7f6ff87c20559be8a3
leosch92 <leonardo_schimpf@id.uff.br>
2018-11-06 12:32:01 -0200

    Input de variantes funcionando (para 1ª e 2ª)

--- __main__.py
+++ __main__.py
@@ -8,27 +8,36 @@ def main():
     numero_correcoes = 2
     populacao_presa_inicial, populacao_predador_inicial = 1000, 100
 
-    opcao = captura_input()
+    metodo, variante = captura_input()
     loop = True
 
     while loop:
-        if opcao in (1, 2):
+        if metodo in (1, 2) and variante in (1, 2):
             solucionador = Solucionador(constantes_temporais, (populacao_presa_inicial, populacao_predador_inicial),
-                                        numero_correcoes, opcao, 1)
+                                        numero_correcoes, metodo, variante)
             solucionador.resolve()
-            opcao = captura_input()
-        elif opcao == 9:
+            metodo, variante = captura_input()
+        elif variante == 3:
+            #TODO implementar terceira variante, incluindo no primeiro if
+            print("Ainda não implementado")
+            metodo, variante = captura_input()
+        elif variante == 9:
             loop = False
         else:
-            opcao = captura_input()
+            metodo, variante = captura_input()
 
 
 def captura_input():
     print("\n")
     try:
-        return int(input("Escolha o método desejado:\n1-BDF3\n2-AM3\n9-Sair\n"))
+        return int(input("Escolha o método desejado:\n1- BDF3\n2- AM3\n")), \
+               int(input("Escolha a variante do problema desejada:\n"
+                         "1- Crescimento da população ilimitado\n"
+                         "2- Coeficiente de fertilidade com comportamente cíclico\n"
+                         "3- Limitação superior à população de presas\n"
+                         "9- Sair\n"))
     except ValueError:
-        return 0
+        return 0, 0
 
 
 if __name__ == "__main__":
