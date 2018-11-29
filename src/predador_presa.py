7e160f27319bf265d09a1c13b659cfa8c7aa5805
João Pedro Sá Medeiros <jpsmedeiros@id.uff.br>
2018-11-29 00:19:30 -0200

    Ajustando parâmetros

--- src/predador_presa.py
+++ src/predador_presa.py
@@ -12,7 +12,8 @@ class PredadorPresa(object):
         self.variante = variante
 
     def f(self, y, t):
-        alpha, beta, gama, delta = (1600/17)*(0.008), 0.008, 0.8, 0.004
+        #alpha, beta, gama, delta = 0.8, 0.004, 0.8, 0.001
+        alpha, beta, gama, delta = 0.8, 0.004, 0.8, 0.001
         aumento_presa = self.define_aumento_presa(y, t, alpha)
         f_presa = aumento_presa - beta * y[0] * y[1]
         f_predador = -gama * y[1] + delta * y[0] * y[1]
